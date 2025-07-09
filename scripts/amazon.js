c92301172d02c5a82d6a5efb1c696b7ec778ff4b
CodeWithXubi <codewithxubi@gmail.com>
2025-07-09 12:14:36 +0530

    Create class version of cart and formatting my code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,10 +1,10 @@
-import {cart,addToCart} from '../data/cart.js';
-import {products} from '../data/products.js'
+import { cart, addToCart } from '../data/cart.js';
+import { products } from '../data/products.js'
 import { formatCurrency } from './utils/money.js';
 
 let productsHTML = '';
 
-products.forEach((product)=> {
+products.forEach((product) => {
   productsHTML += `
   <div class="product-container">
         <div class="product-image-container">
@@ -59,18 +59,18 @@ products.forEach((product)=> {
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-function updateCartQuantity(){
+function updateCartQuantity() {
   let cartQuantity = 0;
 
-  cart.forEach((cartItem)=> {
+  cart.forEach((cartItem) => {
     cartQuantity += cartItem.quantity;
   })
 
   document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
 }
 
-document.querySelectorAll('.js-add-to-cart').forEach((button)=> {
-  button.addEventListener('click', ()=> {
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
     const productId = button.dataset.productId;
     addToCart(productId);
     updateCartQuantity();
